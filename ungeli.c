9c046dda59c39b644dcfbff2335a6dbe02f1435f
Jeff Epler <jepler@unpythonic.net>
2013-11-30 21:39:12 -0600

    Let ungeli do its own computation of the keys
    C's pkcs5v2 is much faster than Python (even a naive implementation)
    and it stops the need to pass around keys on the commandline which is
    good for a feeling of security.

--- ungeli.c
+++ ungeli.c
@@ -34,6 +34,7 @@
 #include <inttypes.h>
 #include <openssl/evp.h>
 #include <openssl/sha.h>
+#include <openssl/md5.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -53,10 +54,18 @@
 #endif
 #endif
 
+#define CRYPTO_AES_XTS (22)
 #define IVSIZE (16)
 #define SHA512_MDLEN (SHA512_DIGEST_LENGTH)
 #define ELI_MAXKEYLEN (64)
 #define ELI_KEY_SHIFT (20)
+#define ELI_SALTLEN (64)
+#define ELI_MAXMKEYS (2)
+#define ELI_DATAKEYLEN (ELI_MAXKEYLEN)
+#define ELI_USERKEYLEN (ELI_MAXKEYLEN)
+#define ELI_IVKEYLEN (ELI_MAXKEYLEN)
+#define ELI_DATAIVKEYLEN (ELI_DATAKEYLEN + ELI_IVKEYLEN)
+#define ELI_MKEYLEN (ELI_DATAIVKEYLEN + SHA512_MDLEN)
 
 static unsigned blocksize = 4096;
 static unsigned long long offset = 0;
@@ -128,6 +137,20 @@ fatalf(const char *fmt, ...) {
     va_end(ap);
     fputc('\n', stderr);
     abort();
+}
+
+void trimnl(char *buf) {
+    char *end = buf + strlen(buf) - 1;
+    while(end >= buf && *end == '\n') { *end-- = 0; }
+}
+
+static void
+putbe32(unsigned char *buf, uint32_t arg)
+{
+    *buf++ = (arg >> 24) & 0xff;
+    *buf++ = (arg >> 16) & 0xff;
+    *buf++ = (arg >>  8) & 0xff;
+    *buf   = (arg      ) & 0xff;
 }
 
 static void
@@ -408,16 +431,214 @@ int is_nbd(int ofd) {
 }
 #endif
 
+static int
+readle16(unsigned char *buf) {
+    int result = *buf++;
+    result |= *buf << 8;
+    return result;
+}
+
+static uint32_t
+readle32(unsigned char *buf) {
+    uint32_t result = *buf++;
+    result |= (uint32_t)*buf++ << 8;
+    result |= (uint32_t)*buf++ << 16;
+    result |= (uint32_t)*buf   << 24;
+    return result;
+}
+
+static uint64_t
+readle64(unsigned char *buf) {
+    uint64_t result = *buf++;
+    result |= (uint64_t)*buf++ << 8;
+    result |= (uint64_t)*buf++ << 16;
+    result |= (uint64_t)*buf++ << 24;
+    result |= (uint64_t)*buf++ << 32;
+    result |= (uint64_t)*buf++ << 40;
+    result |= (uint64_t)*buf++ << 48;
+    result |= (uint64_t)*buf   << 56;
+    return result;
+}
+
+static void
+eli_verify_metadata(unsigned char *buf, unsigned char *hash)
+{
+    unsigned char ckhash[MD5_DIGEST_LENGTH];
+    MD5(buf, 511-16, ckhash);
+
+    if(memcmp(hash, ckhash, sizeof(ckhash)))
+        fatal("metadata hash check failure");
+}
+
+typedef struct {
+    char md_magic[16];
+    uint32_t md_version;
+    uint32_t md_flags;
+    uint16_t md_ealgo;
+    uint16_t md_keylen;
+    uint16_t md_aalgo;
+    uint64_t md_provsize;
+    uint32_t md_sectorsize;
+    uint8_t  md_keys;
+    uint32_t md_iterations;
+    uint8_t  md_salt[ELI_SALTLEN];
+    uint8_t  md_mkeys[ELI_MAXMKEYS * ELI_MKEYLEN];
+    uint8_t  md_hash[MD5_DIGEST_LENGTH];
+} eli_metadata;
+
+static void
+eli_read_metadata(int fd, eli_metadata *md) {
+    unsigned char buf[511], *ptr=buf;
+    lseek(fd, -512, SEEK_END);
+    read_full(fd, buf, 511);
+
+    memcpy(md->md_magic, ptr, sizeof(md->md_magic));
+        ptr += sizeof(md->md_magic);
+    md->md_version = readle32(ptr); ptr += 4;
+    md->md_flags   = readle32(ptr); ptr += 4;
+    md->md_ealgo   = readle16(ptr); ptr += 2;
+    md->md_keylen  = readle16(ptr); ptr += 2;
+    md->md_aalgo   = readle16(ptr); ptr += 2;
+    md->md_provsize = readle64(ptr); ptr += 8;
+    md->md_sectorsize = readle32(ptr); ptr += 4;
+    md->md_keys = *ptr; ptr += 1;
+    md->md_iterations = readle32(ptr); ptr += 4;
+    memcpy(md->md_salt, ptr, sizeof(md->md_salt));
+        ptr += sizeof(md->md_salt);
+    memcpy(md->md_mkeys, ptr, sizeof(md->md_mkeys));
+        ptr += sizeof(md->md_mkeys);
+    memcpy(md->md_hash, ptr, sizeof(md->md_hash));
+        ptr += sizeof(md->md_hash);
+
+    eli_verify_metadata(buf, md->md_hash);
+}
+
+static void
+eli_crypto_decrypt(int ealgo, unsigned char *enckey, size_t keylen,
+        unsigned char *src, size_t len, unsigned char *dest)
+{
+    if(ealgo != CRYPTO_AES_XTS) fatal("unsupported ealgo");
+    if(keylen != 16) fatal("unsupported key length");
+
+    EVP_CIPHER_CTX ctx;
+    EVP_CIPHER_CTX_init(&ctx);
+    EVP_DecryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, enckey, 0);
+    EVP_CIPHER_CTX_set_padding(&ctx, 0);
+    int out_len, final_out_len;
+    EVP_DecryptUpdate(&ctx, dest, &out_len, src, len);
+    EVP_DecryptFinal_ex(&ctx, dest+out_len, &final_out_len);
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    if(out_len + final_out_len != len) fatalf("eli_crypto_decrypt EVP final_out_len %d != %d+%d", len, out_len, final_out_len);
+}
+
+static void pkcs_xorbuf(unsigned char *dest, const unsigned char *src) {
+    for(int i=0; i<SHA512_MDLEN; i++) { dest[i] ^= src[i]; }
+}
+
+static void
+eli_pkcs5v2_genkey(const unsigned char *salt, size_t nsalt, const char *buf, int iterations,
+        unsigned char *dest) {
+    size_t nbuf = strlen(buf);
+    unsigned char saltcount[nsalt+4];
+    memcpy(saltcount, salt, nsalt);
+    putbe32(saltcount+nsalt, 1);
+
+    unsigned char md[SHA512_MDLEN], keyp[SHA512_MDLEN];
+    eli_crypto_hmac((unsigned char*)buf, nbuf,
+            saltcount, nsalt+4, md, sizeof(md));
+    memcpy(keyp, md, sizeof(keyp));
+    for(int i=1; i<iterations; i++) {
+        eli_crypto_hmac((unsigned char*)buf, nbuf,
+                md, SHA512_MDLEN, md, sizeof(md));
+        pkcs_xorbuf(keyp, md);
+    }
+    memcpy(dest, keyp, sizeof(keyp));
+}
+
+static void
+test_eli_pkcs5v2()
+{
+    unsigned char test_hkey[] = {1};
+    char *test_passphrase = "bluemoon";
+    int test_iterations = 8;
+    unsigned char xresult[SHA512_MDLEN];
+    hextobin(xresult, sizeof(xresult),
+        "629e270fe754fb70ce2b2e6bc0de923a0f66ec0d41c03dab6b9049c7446e9d12"
+        "b154f53cd6bc3f32c37436c7c40293e21ea299dfc6617b8e0315b634b0f8474c");
+
+    unsigned char result[SHA512_MDLEN];
+    eli_pkcs5v2_genkey(test_hkey, sizeof(test_hkey), test_passphrase, test_iterations, result);
+
+    if(memcmp(result, xresult, sizeof(xresult))) {
+        fatal("test_eli_crypto_hmac()");
+    }
+}
+
+
+#define MAX(a,b) ((a) < (b) ? (b) : (a))
+
+static int
+eli_mkey_verify(unsigned char *tmpmkey, unsigned char *key) {
+    unsigned char hmkey[SHA512_MDLEN];
+    unsigned char chmac[SHA512_MDLEN];
+    unsigned char *odhmac = tmpmkey + ELI_DATAIVKEYLEN;
+
+    eli_crypto_hmac(key, ELI_USERKEYLEN, (unsigned char*)"\0", 1, hmkey, sizeof(hmkey));
+    eli_crypto_hmac(hmkey, sizeof(hmkey), tmpmkey, ELI_DATAIVKEYLEN, chmac, sizeof(chmac));
+
+    int result = !memcmp(chmac, odhmac, sizeof(chmac));
+    if(!result) fprintf(stderr, "Note: Failed key verification (this is not fatal on its own)\n");
+    return result;
+}
+
+static void
+set_mkey_from_passfile(const char *arg, eli_metadata *md) {
+    FILE *s = fopen(arg, "r");
+    char buf[4096];
+    if(!s) perror_fatal("fopen passfile");
+    fgets(buf, sizeof(buf), s);
+    trimnl(buf);
+    fclose(s);
+
+    unsigned char hbuf[SHA512_MDLEN];
+    eli_pkcs5v2_genkey(md->md_salt, sizeof(md->md_salt),
+            buf, md->md_iterations, hbuf);
+
+    unsigned char key[SHA512_MDLEN];
+    eli_crypto_hmac(0, 0, hbuf, sizeof(hbuf), key, sizeof(key));
+
+    unsigned char enckey[SHA512_MDLEN];
+    eli_crypto_hmac(key, sizeof(key), (unsigned char*)"\1", 1, enckey, sizeof(enckey));
+
+    for(int nkey=0; nkey<ELI_MAXMKEYS; nkey++) {
+        int moff = nkey * ELI_MKEYLEN;
+        int bit = (1<<nkey);
+        if(!(md->md_keys & bit)) continue;
+        unsigned char tmpmkey[ELI_MKEYLEN];
+        eli_crypto_decrypt(md->md_ealgo, enckey, md->md_keylen/8,
+            md->md_mkeys + moff, ELI_MKEYLEN, tmpmkey);
+
+        if(!eli_mkey_verify(tmpmkey, key)) continue;
+        memcpy(mkey, tmpmkey, sizeof(mkey));
+        return;
+    }
+    fatal("Failed to decryt master key");
+}
+
 int main(int argc, char **argv) {
+    char *passphrase_file = NULL;
+
     test_eli_crypto_hmac();
+    test_eli_pkcs5v2();
 
     int opt;
-    while((opt = getopt(argc, argv, "o:n:b:m:")) != -1) {
+    while((opt = getopt(argc, argv, "o:n:b:m:j:")) != -1) {
         switch(opt) {
         case 'o': offset = strtoull(optarg, NULL, 0); break;
         case 'n': nblocks = strtoull(optarg, NULL, 0); break;
         case 'b': blocksize = atoi(optarg); break;
         case 'm': set_mkey(optarg); break;
+        case 'j': passphrase_file = optarg;
         }
     }
 
@@ -436,6 +657,12 @@ int main(int argc, char **argv) {
     if(isatty(ifd))
         fatal("refusing to read from a terminal (use cat | if you insist)");
 
+    eli_metadata md;
+    eli_read_metadata(ifd, &md);
+
+    if(passphrase_file)
+        set_mkey_from_passfile(passphrase_file, &md);
+
     if(is_nbd(ofd)) {
         return setup_nbd(ifd, ofd, blocksize);
     }
@@ -443,9 +670,8 @@ int main(int argc, char **argv) {
     if(isatty(ofd))
         fatal("refusing to write to a terminal (use | cat if you insist)");
 
-    if(offset)
-        if(lseek(ifd, offset*blocksize, SEEK_SET) < 0)
-            perror_fatal("lseek(input)");
+    if(lseek(ifd, offset*blocksize, SEEK_SET) < 0)
+        perror_fatal("lseek(input)");
     
     for(unsigned long long i=0; i<nblocks; i++)
     {
